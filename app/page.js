0fc560e5a066d1d7c6505d3d69ee889ada376ad8
Parag Agarwal <paragagarwal0589@gmail.com>
2023-11-02 17:24:50 +0530

    Routs(Link-tag)

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
 "use client"
 import React, { useState } from 'react';
 import Hader from '@/Components/Hader';
+import Link from 'next/link';
 
 const page = () => {
   return (
     <>
     <Hader name={'Parag'} surname={'Agarwal'}/>
-    
+    <Link href= "/About">About</Link>
+    <Link href="/Courses">Cources</Link>
 
-
-    
     </>
   )
 }
