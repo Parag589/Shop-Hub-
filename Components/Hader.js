9181145c6bcd22d9492883fb34de7d6bbcce4baa
Parag Agarwal <paragagarwal0589@gmail.com>
2023-11-02 18:24:50 +0530

    useAPI

--- Components/Hader.js
+++ Components/Hader.js
@@ -1,5 +1,7 @@
 "use client"
+import axios from 'axios';
 import React, { useState } from 'react';
+
 
 const Hader = (props) => {
   // const name = "parag";
--- app/page.js
+++ app/page.js
@@ -2,13 +2,45 @@
 import React, { useState } from 'react';
 import Hader from '@/Components/Hader';
 import Link from 'next/link';
+import axios from 'axios';
 
 const page = () => {
+  const [Images, setImages] = useState([])
+
+  const getImages = async () =>{
+
+    // correct the code
+
+    
+    try{
+      const response = await axios.get("https://picsum.photos/v2/list")
+      const data = response.data;
+      setImages(data);
+      console.log(Images);
+    } catch(error){
+      console.log("ERROR");
+    }
+
+  }
   return (
     <>
     <Hader name={'Parag'} surname={'Agarwal'}/>
     <Link href= "/About">About</Link>
     <Link href="/Courses">Cources</Link>
+
+      <button onClick={getImages} className='px-3 py-3 bg-green-700'>Get Images</button>
+      
+      <div>
+        {Images.map((elem, i) => {
+           return<img 
+           key={i}
+           src={elem.download_url}
+           width={300}
+           height={300}
+           className='inline-block'
+           />
+        })}
+      </div>
 
     </>
   )
