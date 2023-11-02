14df5cbbb676dd49d83e819128dc33bfcfe644f2
Parag Agarwal <paragagarwal0589@gmail.com>
2023-11-02 14:41:06 +0530

    useState

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 
 const page = () => {
-  const name = "parag";
+  // const name = "parag";
+  const[name, setName] = useState(Parag)
+//      ^     ^ Function Name
+//      variable  
+
   return (
     // fragments
      <>
     <h1 className='font-bold'>About {name}</h1>
+    <button className='bg-slate-600' onClick={()=>setName('Parag2')}>Change Name</button>
     </>
   )
 }
